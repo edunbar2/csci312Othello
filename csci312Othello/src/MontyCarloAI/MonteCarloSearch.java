69ec270b734b18fe8e3059bbaf4127f209223da6
Eric Dunbar <edunbar@unca.edu>
2021-10-15 14:43:13 -0400

    updated name of MonteCarloSearch.java

---
+++ csci312Othello/src/MontyCarloAI/MonteCarloSearch.java
@@ -0,0 +1,37 @@
+package MontyCarloAI;
+/**
+ * @Author Eric Dunbar
+ * Inspiration from
+ */
+
+import edu.unca.csci312.*;
+public class MonteCarloSearch {
+    private static final int WINSCORE = 10;
+    private static final int endTime = 90;
+    private int level;
+    private int opponent; //1 or -1
+
+    public Board findNextMove(Board currentBoard, int player){
+        opponent = player * -1;
+        Tree tree = new Tree();
+        Node rootNode = tree.getRoot();
+        rootNode.getState().setBoard(currentBoard);
+        rootNode.getState().setPlayer(opponent);
+        double startTime = System.currentTimeMillis()/1000;
+        while(((System.currentTimeMillis()/1000) - startTime) < endTime){
+            Node promisingNode = selectPromisingNode(rootNode);
+            if(!Board.gameOver(promisingNode.getState().getBoard()))
+                expandNode(promisingNode);
+            Node nodeToExplore = promisingNode;
+        }
+        return null;
+    }
+
+    private Node selectPromisingNode(Node rootNode){
+        Node node = rootNode;
+        while(node.getChildren().size() != 0){
+            node = UCT.findBestNodeWithUCT(node);
+        }
+        return node;
+    }
+}
